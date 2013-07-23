89613643c3ded57e237ddca94f0c4499494405d5
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2013-07-23 21:23:02 +0900

    Add other-modules entry to the cabal file

--- src/Network/JobQueue/Class.hs
+++ src/Network/JobQueue/Class.hs
@@ -1,12 +1,13 @@
 
 module Network.JobQueue.Class where
 
-class (Read a, Show a, Desc a) => Unit a where
-  getPriority :: a -> Int
-  getRecovery :: a -> a
-
 class (Show a) => Desc a where
   desc :: a -> String
   desc x = show x
   shortDesc :: a -> String
   shortDesc x = takeWhile (/= ' ') $ show x
+
+class (Read a, Show a, Desc a) => Unit a where
+  getPriority :: a -> Int
+  getRecovery :: a -> a
+
