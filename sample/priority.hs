371c0eee6f7c4ef484408d3d953e6e8be2138299
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2014-02-04 13:45:09 +0900

    Fix some queries by inserting table names with string concatenation

--- sample/priority.hs
+++ sample/priority.hs
@@ -8,11 +8,12 @@ data JobEnv = JobEnv {
 
 instance Env JobEnv where
 
-data JobUnit = Priority0 | Priority1 | Priority2 | Failed deriving (Show, Read, Eq, Ord)
+data JobUnit = Priority0 | Priority1 Int | Priority1' Int | Priority2 | Failed deriving (Show, Read, Eq, Ord)
 
 instance Unit JobUnit where
   getPriority Priority0 = -1
-  getPriority Priority1 = 0
+  getPriority Priority1 {} = 0
+  getPriority Priority1' {} = 0
   getPriority Priority2 = 1
   getPriority Failed    = -2
   getRecovery _ju = Failed
@@ -26,12 +27,14 @@ main = do
     (loc:name:args') -> do
       let withJobQueue = buildJobQueue loc name $ do
             process $ \Priority0 -> commitIO (putStrLn "0") >> fin
-            process $ \Priority1 -> commitIO (putStrLn "1") >> fin
+            process $ \(Priority1 ttl) -> commitIO (putStrLn "1") >> if ttl > 0 then fork $ Priority1 (ttl-1) else fin
+            process $ \(Priority1' ttl) -> commitIO (putStrLn "1'") >> if ttl > 0 then fork $ Priority1' (ttl-1) else fin
             process $ \Priority2 -> commitIO (putStrLn "2") >> fin
             process $ \Failed    -> commitIO (putStrLn "failed.") >> fin
       case args' of
         ("run":[]) -> withJobQueue $ loop (JobEnv)
-        ("init":[]) -> withJobQueue $ \jq -> mapM_ (scheduleJob jq) [Priority1, Priority2, Priority0]
+        ("init":[]) -> withJobQueue $ \jq -> mapM_ (scheduleJob jq) [Priority1 10, Priority1' 10, Priority2, Priority0]
+        ("show":[]) -> withJobQueue $ loop (JobEnv)
         (cmd:_) -> putStrLn $ "unknown command: " ++ cmd
     _ -> return ()
   where
--- src/Network/JobQueue/Backend/Sqlite3.hs
+++ src/Network/JobQueue/Backend/Sqlite3.hs
@@ -26,7 +26,7 @@ openSqlite3Backend filePath = do
   conn <- connectSqlite3 filePath
   return $ Backend {
       bOpenQueue = \queueName -> do
-         run conn "CREATE TABLE IF NOT EXISTS ? (key INTEGER PRIMARY KEY AUTOINCREMENT, prio INTEGER, value TEXT, version INTEGER)" [toSql queueName]
+         run conn ("CREATE TABLE IF NOT EXISTS " ++ queueName ++ " (key INTEGER PRIMARY KEY AUTOINCREMENT, prio INTEGER, value TEXT, version INTEGER)") []
          return (Sqlite3Queue conn queueName)
     , bClose = \_ -> disconnect conn
     }
@@ -39,59 +39,61 @@ newSqlite3Backend conn = Backend {
 
 readDBQueue :: Sqlite3Queue -> IO (Maybe (BS.ByteString, String))
 readDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
-  sqlvalues <- quickQuery conn' "SELECT key, value FROM ? ORDER BY prio, key desc LIMIT 1" [toSql queueName]
+  sqlvalues <- quickQuery conn' ("SELECT key, value FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
     [] -> return (Nothing)
     ((key:value:_):_) -> do
-      run conn' "DELETE FROM ? WHERE key = ?" [toSql queueName, key]
+      run conn' ("DELETE FROM " ++ queueName ++ " WHERE key = ?") [toSql key]
       return (Just (fromSql value, fromSql key))
 
 peekDBQueue :: Sqlite3Queue -> IO (Maybe (BS.ByteString, String, String, Int))
 peekDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
-  sqlvalues <- quickQuery conn' "SELECT key, value, version FROM ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName]
+  sqlvalues <- quickQuery conn' ("SELECT key, value, version FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
     [] -> return (Nothing)
     ((key:value:version:_):_) -> return (Just (fromSql value, fromSql key, fromSql key, fromSql version))
 
 writeDBQueue :: Sqlite3Queue -> BS.ByteString -> Int -> IO (String)
-writeDBQueue (Sqlite3Queue conn queueName) value prio = withTransaction conn $ \conn' -> do
-  run conn' "INSERT INTO ?(prio, value, version) VALUES (?,?,0)" [toSql queueName, toSql prio, toSql value]
-  sqlvalues <- quickQuery conn' "SELECT key FROM ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName]
-  case sqlvalues of
-    [] -> return ("")
-    ((key:_):_) -> do
-      return (fromSql key)
+writeDBQueue (Sqlite3Queue conn queueName) value prio = do
+  withTransaction conn $ \conn' -> do
+    _ <- run conn' ("INSERT INTO " ++ queueName ++ "(prio, value, version) VALUES (?,?,0)") [toSql prio, toSql value]
+    sqlvalues <- quickQuery conn' ("SELECT seq FROM sqlite_sequence where name = '" ++ queueName ++ "'") []
+    case sqlvalues of
+      [] -> return ("")
+      ((key:_):_) -> do
+        return (fromSql key)
 
 deleteDBQueue :: Sqlite3Queue -> String -> IO (Bool)
 deleteDBQueue (Sqlite3Queue conn queueName) key = withTransaction conn $ \conn' -> do
-  run conn' ("DELETE FROM ? WHERE key = ?") [toSql queueName, toSql key]
+  _ <- run conn' ("DELETE FROM " ++ queueName ++ " WHERE key = ?") [toSql key]
   return (True)
 
 updateDBQueue :: Sqlite3Queue -> String -> BS.ByteString -> Int -> IO (Bool)
-updateDBQueue (Sqlite3Queue conn queueName) key value version = withTransaction conn $ \conn' -> do
-  run conn' "UPDATE ? SET value = ?, version = ? WHERE key = ? AND version = ?" [toSql queueName, toSql value, toSql version, toSql key, toSql version]
-  sqlvalues <- quickQuery conn' "SELECT key FROM ? WHERE key = ? AND version = ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName, toSql key, toSql (version+1)]
-  case sqlvalues of
-    [] -> return (False)
-    _ -> return (True)
+updateDBQueue (Sqlite3Queue conn queueName) key value version = do
+  withTransaction conn $ \conn' -> do
+    _ <- run conn' ("UPDATE " ++ queueName ++ " SET value = ?, version = ? WHERE key = ? AND version = ?") [toSql value, toSql (version+1), toSql key, toSql version]
+    sqlvalues <- quickQuery conn' ("SELECT key FROM " ++ queueName ++ " WHERE key = ? AND version = ? ORDER BY prio, key LIMIT 1") [toSql key, toSql (version+1)]
+    case sqlvalues of
+      [] -> return (False)
+      _ -> return (True)
 
 countDBQueue :: Sqlite3Queue -> IO (Int)
 countDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
-  sqlvalues <- quickQuery conn' "SELECT COUNT (*) FROM ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName]
+  sqlvalues <- quickQuery conn' ("SELECT COUNT (*) FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
     [] -> return (0)
     ((count:_):_) -> return (fromSql count)
 
 itemsDBQueue :: Sqlite3Queue -> IO ([String])
 itemsDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
-  sqlvalues <- quickQuery conn' "SELECT COUNT (*) FROM ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName]
+  sqlvalues <- quickQuery conn' ("SELECT COUNT (*) FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
     [] -> return ([])
     (keys:_) -> return (map fromSql keys)
 
 listDBQueue :: Sqlite3Queue -> IO ([BS.ByteString])
 listDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
-  sqlvalues <- quickQuery conn' "SELECT COUNT (*) FROM ? ORDER BY prio, key DESC LIMIT 1" [toSql queueName]
+  sqlvalues <- quickQuery conn' ("SELECT COUNT (*) FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
     [] -> return ([])
     (keys:_) -> return (map fromSql keys)
