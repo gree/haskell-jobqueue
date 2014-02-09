1d62c35bce1dcb3f269147fd473e97c9e77b3eb9
Kiyoshi IKEHARA <kikehara@gmail.com>
2014-02-09 16:57:38 +0900

    Fix non-exhaustive pattern patches

--- src/Network/JobQueue/Backend/Sqlite3.hs
+++ src/Network/JobQueue/Backend/Sqlite3.hs
@@ -26,7 +26,7 @@ openSqlite3Backend filePath = do
   conn <- connectSqlite3 filePath
   return $ Backend {
       bOpenQueue = \queueName -> do
-         run conn ("CREATE TABLE IF NOT EXISTS " ++ queueName ++ " (key INTEGER PRIMARY KEY AUTOINCREMENT, prio INTEGER, value TEXT, version INTEGER)") []
+         _ <- run conn ("CREATE TABLE IF NOT EXISTS " ++ queueName ++ " (key INTEGER PRIMARY KEY AUTOINCREMENT, prio INTEGER, value TEXT, version INTEGER)") []
          return (Sqlite3Queue conn queueName)
     , bClose = \_ -> disconnect conn
     }
@@ -41,17 +41,17 @@ readDBQueue :: Sqlite3Queue -> IO (Maybe (BS.ByteString, String))
 readDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
   sqlvalues <- quickQuery conn' ("SELECT key, value FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
-    [] -> return (Nothing)
     ((key:value:_):_) -> do
-      run conn' ("DELETE FROM " ++ queueName ++ " WHERE key = ?") [toSql key]
+      _ <- run conn' ("DELETE FROM " ++ queueName ++ " WHERE key = ?") [toSql key]
       return (Just (fromSql value, fromSql key))
+    _ -> return (Nothing)
 
 peekDBQueue :: Sqlite3Queue -> IO (Maybe (BS.ByteString, String, String, Int))
 peekDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
   sqlvalues <- quickQuery conn' ("SELECT key, value, version FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
-    [] -> return (Nothing)
     ((key:value:version:_):_) -> return (Just (fromSql value, fromSql key, fromSql key, fromSql version))
+    _ -> return (Nothing)
 
 writeDBQueue :: Sqlite3Queue -> BS.ByteString -> Int -> IO (String)
 writeDBQueue (Sqlite3Queue conn queueName) value prio = do
@@ -59,9 +59,9 @@ writeDBQueue (Sqlite3Queue conn queueName) value prio = do
     _ <- run conn' ("INSERT INTO " ++ queueName ++ "(prio, value, version) VALUES (?,?,0)") [toSql prio, toSql value]
     sqlvalues <- quickQuery conn' ("SELECT seq FROM sqlite_sequence where name = '" ++ queueName ++ "'") []
     case sqlvalues of
-      [] -> return ("")
       ((key:_):_) -> do
         return (fromSql key)
+      _ -> return ("")
 
 deleteDBQueue :: Sqlite3Queue -> String -> IO (Bool)
 deleteDBQueue (Sqlite3Queue conn queueName) key = withTransaction conn $ \conn' -> do
@@ -81,8 +81,8 @@ countDBQueue :: Sqlite3Queue -> IO (Int)
 countDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
   sqlvalues <- quickQuery conn' ("SELECT COUNT (*) FROM " ++ queueName ++ " ORDER BY prio, key LIMIT 1") []
   case sqlvalues of
-    [] -> return (0)
     ((count:_):_) -> return (fromSql count)
+    _ -> return (0)
 
 itemsDBQueue :: Sqlite3Queue -> IO ([String])
 itemsDBQueue (Sqlite3Queue conn queueName) = withTransaction conn $ \conn' -> do
