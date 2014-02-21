4599935456829490cbaec7c570a5b9a830431dcf
Kiyoshi Ikehara <kiyoshi.ikehara@gree.net>
2014-02-21 16:44:59 +0900

    Add a test case for suspend

--- test/test-jobqueue.hs
+++ test/test-jobqueue.hs
@@ -9,8 +9,6 @@ import Control.Exception
 import Test.Framework.TH
 import Test.Framework.Providers.HUnit
 import Test.HUnit
-import qualified Data.ByteString.Char8 as BS
-import System.Environment hiding (getEnv)
 import Control.Monad
 import System.Directory
 import System.IO.Error (isDoesNotExistError)
@@ -37,6 +35,7 @@ instance Desc JobUnit where
 
 case_hello_1 :: IO ()
 case_hello_1 = do
+  removeIfExists "case.sqlite3"
   let withJobQueue = buildJobQueue "sqlite3://case.sqlite3" "/hello_1" $ do
         process $ \WorldStep -> commitIO (putStrLn "world") >> fin
         process $ \HelloStep -> do
@@ -58,7 +57,40 @@ case_hello_1 = do
       count <- countJobQueue jq
       when (count > 0) $ loop env jq
 
+case_suspend_1 :: IO ()
+case_suspend_1 = do
+  removeIfExists "case.sqlite3"
+  let withJobQueue = buildJobQueue "sqlite3://case.sqlite3" "/hello_1" $ do
+        process $ \WorldStep -> commitIO (putStrLn "world") >> fin
+        process $ \HelloStep -> do
+          env <- getEnv
+          commitIO (putStr $ (jeHello env) ++ ", ")
+          next WorldStep
+  withJobQueue $ \jq -> do
+    scheduleJob jq HelloStep
+    r <- suspendJobQueue jq
+    r @?= True
+    r' <- suspendJobQueue jq -- should be ignored
+    r' @?= False
+    step (JobEnv "hello") jq 5
+    count <- countJobQueue jq
+    count @?= 2
+  withJobQueue $ \jq -> do
+    r <- resumeJobQueue jq
+    r @?= True
+    step (JobEnv "hello") jq 5
+    count <- countJobQueue jq
+    count @?= 0
+  removeIfExists "case.sqlite3"
+
 ---------------------------------------------------------------- Utils
+
+step :: (Unit a1, Env e) => e -> JobQueue e a1 -> Int -> IO ()
+step env jq c
+  | c > 0 = do
+    executeJob jq env
+    step env jq (pred c)
+  | otherwise = return ()
 
 removeIfExists :: FilePath -> IO ()
 removeIfExists fileName = removeFile fileName `catch` handleExists
